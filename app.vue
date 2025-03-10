b9ccd1057644723604029aa31c0312b4d27035aa
Mxmac <pro.maximeandre@gmail.com>
2025-03-10 21:30:29 +0100

    test de backgrounds

--- app.vue
+++ app.vue
@@ -15,11 +15,25 @@ import Sidebar from "@/components/Sidebar.vue";
 .app-container {
   display: flex;
   height: 100vh;
+  background: rgba(88, 81, 59, 0.651);
+  /*background-image: url(/public/icons/IMG_3666.png);*/
+  background-size: cover;
 }
+
 
 .content {
   flex: 1; 
-
+  display: flex;
+  flex-direction: column;
+  background: url(/public/icons/background.png) no-repeat center center;
+  background-size:100%;
   overflow-x: hidden; 
+}
+
+@media screen and (min-width: 1024px){
+.content{
+  background-size:35%;
+}
+  
 }
 </style>
