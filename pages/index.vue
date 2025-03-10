8aa19ec4aa2b852710887317b220c6c232c1b5c2
Mxmac <pro.maximeandre@gmail.com>
2025-03-10 21:31:37 +0100

    minor change

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <header>
     <img src="/icons/logo.png" id="logo" alt="logo de l'app" width="250" height="250">
- 
   </header>
   <FooterComp />
 </template>
@@ -11,7 +10,7 @@
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Jersey+10&display=swap');
+
 header{
   display: flex;
   width: 100%;
