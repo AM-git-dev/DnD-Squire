7ce79254d107b24ae56c34b27fd2d493df2543dd
Mxmac <pro.maximeandre@gmail.com>
2025-03-10 15:10:29 +0100

    retrait de la navbar au profit d'une sidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="app-container">
+    <Sidebar />
+    <div class="content">
+      <NuxtPage />
+    </div>
   </div>
 </template>
+
+<script setup>
+import Sidebar from "@/components/Sidebar.vue";
+</script>
+
+<style scoped>
+.app-container {
+  display: flex;
+  height: 100vh;
+}
+
+.content {
+  flex: 1; 
+  padding: 20px; 
+  overflow-x: hidden; 
+}
+</style>
