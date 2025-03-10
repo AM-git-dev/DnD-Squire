118765e35b33dd79406f1a7cca30ab805e3c52e3
Mxmac <pro.maximeandre@gmail.com>
2025-03-10 12:13:51 +0100

    ajout de 2 boutons (ajout suppression) a la page des personnages, avec un objet pour test

--- pages/characters.vue
+++ pages/characters.vue
@@ -2,7 +2,14 @@
   <div>
     <Navbar />
     <h1>Gestion des personnages</h1>
-    <p>Voici la liste de tes personnages de jeu de rôle.</p>
+    <Button variant="create" @click="addCharacter">Créer un personnage</Button>
+    <Button variant="delete" @click="deleteCharacter">Supprimer un personnage</Button>
+    <p>Voici la liste de tes personnages de jeu de rôle :</p>
+
+    <ul>
+      <li v-for="character in characters">{{ character.name }} - Niveau {{ character.level }}</li>
+    </ul>
+
 
   </div>
   <FooterComp />
@@ -10,7 +17,22 @@
 
 
 <script setup>
+import Navbar from '@/components/Navbar.vue';
+import Button from '@/components/Button.vue';
 
+
+const characters = ref ([
+  { name: 'Hugo', level:1},
+  { name: 'Maxime', level:1}
+])
+
+const addCharacter = () => {
+  characters.value.push({name: prompt("Quel est le nom du personnage ?"), level:1})
+}
+
+const deleteCharacter = () => {
+  characters.value.pop()
+}
 </script>
 
 <style>
