260cc586962011215ff8a692d51d81161d86b4ae
Mxmac <pro.maximeandre@gmail.com>
2025-03-10 15:49:05 +0100

    sécurisation des données sensibles

---
+++ plugins/firebase.client.js
@@ -0,0 +1,21 @@
+import { defineNuxtPlugin, useRuntimeConfig } from '#app';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
+  const firebaseConfig = {
+    apiKey: config.public.firebaseApiKey,
+    authDomain: config.public.firebaseAuthDomain,
+    projectId: config.public.firebaseProjectId,
+    storageBucket: config.public.firebaseStorageBucket,
+    messagingSenderId: config.public.firebaseMessagingSenderId,
+    appId: config.public.firebaseAppId
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  nuxtApp.provide('firebaseDb', db);
+});
