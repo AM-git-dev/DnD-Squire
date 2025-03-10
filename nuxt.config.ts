3141be4eef72bfa1f66f0db1cc82096adfed9c5c
Mxmac <pro.maximeandre@gmail.com>
2025-03-10 15:48:35 +0100

    passage des variables d'environnement

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,17 @@
     compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   
-  css: ["@/assets/styles.css"]
-
+  css: ["@/assets/styles.css"],
+runtimeConfig: {
+  public: {
+    firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+  }
+}
   })
   
 
