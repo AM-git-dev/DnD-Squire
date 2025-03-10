9a531b3f3a20eb602cb7f07cd9f0491adb5102e1
Mxmac <pro.maximeandre@gmail.com>
2025-03-10 10:52:12 +0100

    creation des premiers composants et de la feuille de style globale

---
+++ components/FooterComp.vue
@@ -0,0 +1,15 @@
+<template>
+<footer>
+  <span>
+    D&D Mobile Companion | 2025 Tous Droits Reservés
+  </span>
+</footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <NuxtLink to="/">Accueil</NuxtLink>
+    <NuxtLink to="/characters">Personnages</NuxtLink>
+    <NuxtLink to="/inventory">Inventaire</NuxtLink>
+  </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
