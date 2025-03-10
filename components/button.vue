3af44c3cd103dd0dce2ff017d44c9621789991a8
Mxmac <pro.maximeandre@gmail.com>
2025-03-10 12:12:33 +0100

    creation d'un bouton reutilisable

---
+++ components/button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :class="variant" @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+
+defineProps({
+  variant: { type: String, default: 'default' }
+});
+const emit = defineEmits(['click']);
+const onClick = () => emit('click');
+
+</script>
+
+<style scoped>
+
+button {
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.default {
+  background: gray;
+  color: white;
+}
+
+.create {
+  background: blue;
+  color: white;
+}
+
+.delete {
+  background: red;
+  color: white;
+}
+</style>
