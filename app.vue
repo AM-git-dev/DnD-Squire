9a96e424f31a2cc6f5e533c2672485d1efb8c5ff
mix35 <pro.maximeandre@gmail.com>
2025-03-13 07:47:09 +0100

    import du store character + some style

--- app.vue
+++ app.vue
@@ -9,30 +9,41 @@
 
 <script setup>
 import Sidebar from "@/components/Sidebar.vue";
+import {useCharacterStore} from '@/stores/character';
+const characterStore = useCharacterStore();
+characterStore.loadCharacterFromStorage();
+
+
 </script>
 
 <style scoped>
 .app-container {
   display: flex;
   height: 100vh;
-  background: rgba(88, 81, 59, 0.651);
-  /*background-image: url(/public/icons/IMG_3666.png);*/
+  background: rgba(88, 81, 59, 0.651) url(/icons/testbackground.png);
   background-size: cover;
+  background-position: bottom center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 
 
+
 .content {
-  flex: 1; 
+  flex: 1;
   display: flex;
   flex-direction: column;
-  background: url(/public/icons/background.png) no-repeat center center;
+  /*background: url(/public/icons/background.png) no-repeat center center;*/
   background-size:100%;
-  overflow-x: hidden; 
+  overflow-x: hidden;
 }
 
 @media screen and (min-width: 1024px){
 .content{
-  background-size:35%;
+  display:flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height:100%
 }
   
 }
