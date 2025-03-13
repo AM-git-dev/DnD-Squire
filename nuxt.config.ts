80e5b5d7d47d26f7df39c311c071a6dd0c03c2fb
mix35 <pro.maximeandre@gmail.com>
2025-03-13 07:50:36 +0100

    ajout de pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,9 @@
     },
     compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  
+  modules:[
+      '@pinia/nuxt'
+  ],
   css: ["@/assets/styles.css"],
 runtimeConfig: {
   public: {
