1bc6a8a2e73536a7bf7ee7974fbad3501e69cdda
Mxmac <pro.maximeandre@gmail.com>
2025-03-14 16:48:21 +0100

    endpoint character/create

---
+++ pages/characters/create.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="characters-create-page">
+    <div class="container mx-auto py-8">
+      <CharacterCreation />
+    </div>
+  </div>
+</template>
+
+<script>
+import CharacterCreation from '@/components/charactermanagement/CharacterCreation.vue';
+
+export default {
+  name: 'CharactersCreatePage',
+  components: {
+    CharacterCreation
+  },
+  head() {
+    return {
+      title: 'Créer un personnage - D&D Companion'
+    };
+  }
+};
+</script>
+
+<style scoped>
+.characters-create-page {
+  min-height: 100vh;
+  background-color: #f8f9fa00;
+}
+</style>  
